7e934c94b3ac5556a668c24b630479a5afc4644f
arufiand <alfiandannyarmanta@gmail.com>
2024-09-29 15:35:54 +0700

    feat: complete version of Dockerized container apps
    Author: @arufiand

--- posts/index.js
+++ posts/index.js
@@ -1,28 +1,45 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser');
-const app = express();
-const posts = {};
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
 
-    posts[id] = {
-        id, title
-    };
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    res.status(201).send(posts[id]);
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post("http://host.docker.internal:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('listeing on 4000');
-
+  console.log("Listening on 4000");
 });
